a2e60f3cad8c90dc104bb46ddb03cf32fc4ed0ab
Simon Heybrock <simon.heybrock@ess.eu>
2024-07-22 11:40:22 +0200

    Add not on two-step binning

--- src/ess/powder/grouping.py
+++ src/ess/powder/grouping.py
@@ -2,8 +2,6 @@
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 """Grouping and merging of pixels / voxels."""
 
-import scipp as sc
-
 from .types import (
     DspacingBins,
     DspacingData,
@@ -12,23 +10,6 @@ from .types import (
     RunType,
     TwoThetaBins,
 )
-
-
-def _drop_grouping_and_bin(
-    data: sc.DataArray, *, dims_to_reduce: tuple[str, ...] | None = None, edges: dict
-) -> sc.DataArray:
-    all_pixels = data if dims_to_reduce == () else data.bins.concat(dims_to_reduce)
-    # all_pixels may just have a single bin now, which currently yields
-    # inferior performance when binning (no/bad multi-threading?).
-    # We operate on the content buffer for better multi-threaded performance.
-    if all_pixels.ndim == 0:
-        return (
-            all_pixels.value.bin(**edges)
-            .assign_coords(all_pixels.coords)
-            .assign_masks(all_pixels.masks)
-        )
-    else:
-        return all_pixels.bin(**edges)
 
 
 def focus_data_dspacing(
@@ -44,10 +25,12 @@ def focus_data_dspacing_and_two_theta(
     dspacing_bins: DspacingBins,
     twotheta_bins: TwoThetaBins,
 ) -> FocussedDataDspacingTwoTheta[RunType]:
+    # Note the binning in two steps: Since two_theta is only pixel-dependent it is
+    # more efficient, since it avoids having to map two_theta to each event, or a
+    # costly many-to-many binning operation.
     return FocussedDataDspacingTwoTheta[RunType](
-        data.bin(
-            {twotheta_bins.dim: twotheta_bins, dspacing_bins.dim: dspacing_bins},
-            dim=data.dims,
+        data.bin({twotheta_bins.dim: twotheta_bins}).bin(
+            {dspacing_bins.dim: dspacing_bins}
         )
     )
 
