749313468c84094f1cfe491214fcf06475473faf
Johannes Kasimir <johanneskasimir@gmail.com>
2025-07-02 10:21:44 +0200

    docs: add ground truth peak positions to notebook

--- src/ess/beer/data.py
+++ src/ess/beer/data.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
 """Data for tests and documentation with BEER."""
 
+import scipp as sc
+
 _version = "1"
 
 __all__ = ["mcstas_duplex_medium_resolution", "mcstas_silicon_medium_resolution"]
@@ -18,6 +20,8 @@ def _make_pooch():
         registry={
             "duplex.h5": "md5:ebb3f9694ffdd0949f342bd0deaaf627",
             "silicon.h5": "md5:3425ae2b2fe7a938c6f0a4afeb9e0819",
+            "silicon-dhkl.tab": "md5:90bedceb23245b045ce1ed0170c1313b",
+            "duplex-dhkl.tab": "md5:b4c6c2fcd66466ad291f306b2d6b346e",
         },
     )
 
@@ -39,3 +43,29 @@ def mcstas_silicon_medium_resolution() -> str:
     medium resolution chopper configuration.
     """
     return _pooch.fetch('silicon.h5')
+
+
+def duplex_peaks() -> str:
+    return _pooch.fetch('duplex-dhkl.tab')
+
+
+def silicon_peaks() -> str:
+    return _pooch.fetch('silicon-dhkl.tab')
+
+
+def duplex_peaks_array() -> sc.Variable:
+    with open(duplex_peaks()) as f:
+        return sc.array(
+            dims='d',
+            values=sorted([float(x) for x in f.read().split('\n') if x != '']),
+            unit='angstrom',
+        )
+
+
+def silicon_peaks_array() -> sc.Variable:
+    with open(silicon_peaks()) as f:
+        return sc.array(
+            dims='d',
+            values=sorted([float(x) for x in f.read().split('\n') if x != '']),
+            unit='angstrom',
+        )
