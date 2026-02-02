b9959ae82721b0d183ca8aa2f8593cd693d8bf6d
Johannes Kasimir <johanneskasimir@gmail.com>
2026-02-02 13:23:48 +0100

    feat: expose NC options in peak reader

--- src/ess/diffraction/peaks.py
+++ src/ess/diffraction/peaks.py
@@ -2,9 +2,9 @@ import NCrystal as NC
 import scipp as sc
 
 
-def dspacing_peak_positions_from_cif(cif, intensity_threshold=None) -> sc.Variable:
+def dspacing_peaks_from_cif(cif, intensity_threshold=None, **kwargs) -> sc.DataArray:
     """
-    Retrieves a list of the peak positions for the given material.
+    Retrieves a data array representing the bragg peaks of the given material.
 
     The material is represented by a cif file or a codid or similar.
     The number of peaks retrieved can be controlled by setting the intensity
@@ -27,20 +27,36 @@ def dspacing_peak_positions_from_cif(cif, intensity_threshold=None) -> sc.Variab
         times the multiplicity of the peak.
         The ``intensity_threshold`` must be convertible to unit ``barn``.
 
+    kwargs:
+        Can be anything that :py:`NCrystal.NCMATComposer.from_cif` supports.
+        For example: `uiso_temperature` or `override_spacegroup`.
+
     Returns
     ------------
         Array containing the peak positions in `dspacing`, with unit ``angstrom``.
     """
-    info = NC.NCMATComposer.from_cif(cif).load('comp=bragg').info
+    info = NC.NCMATComposer.from_cif(cif, **kwargs).load('comp=bragg').info
     min_intensity = (
         intensity_threshold.to(unit='barn').value
         if intensity_threshold is not None
         else 0
     )
-    return sc.array(
-        dims=['peaks'],
-        values=[
-            hkl.d for hkl in info.hklObjects() if (hkl.f2 * hkl.mult) >= min_intensity
-        ],
+    dims = ['peaks']
+    peaks = [hkl for hkl in info.hklObjects() if (hkl.f2 * hkl.mult) >= min_intensity]
+    dspacing = sc.array(
+        dims=dims,
+        values=[hkl.d for hkl in peaks],
         unit='angstrom',
     )
+    out = sc.DataArray(
+        sc.array(dims=dims, values=[hkl.f2 * hkl.mult for hkl in peaks], unit='barn'),
+        coords={
+            'dspacing': dspacing,
+            'cif': sc.scalar(cif),
+            'info': sc.scalar(info),
+            **{name: sc.scalar(value) for name, value in kwargs.items()},
+        },
+    )
+    if intensity_threshold is not None:
+        out.coords['intensity_threshold'] = intensity_threshold
+    return out
