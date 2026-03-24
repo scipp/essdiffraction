715c38983ab2c78a597df47621f21351e87b26e5
Johannes Kasimir <johanneskasimir@gmail.com>
2026-03-24 15:25:14 +0100

    refactor: better name for chopper delay

--- src/ess/beer/conversions.py
+++ src/ess/beer/conversions.py
@@ -18,7 +18,7 @@ from .types import (
 
 def compute_tof_in_each_cluster(
     da: StreakClusteredData[RunType],
-    time0: WavelengthDefinitionChopperDelay,
+    chopper_delay: WavelengthDefinitionChopperDelay,
     mod_period: ModulationPeriod,
 ) -> TofDetector[RunType]:
     """Fits a line through each cluster, the intercept of the line is t0.
@@ -55,7 +55,7 @@ def compute_tof_in_each_cluster(
             too_far_from_center=(distance_to_self > max_distance_from_streak_line),
         )
 
-    t0 = _round_t0_to_nearest_chopper_opening(sc.values(t0), mod_period, time0)
+    t0 = _round_t0_to_nearest_chopper_opening(sc.values(t0), mod_period, chopper_delay)
     da = da.assign_coords(t0=t0)
     da = da.bins.assign_coords(tof=(t - t0))
     return da
@@ -64,15 +64,14 @@ def compute_tof_in_each_cluster(
 def _round_t0_to_nearest_chopper_opening(
     t0: sc.Variable,
     mod_period: sc.Variable,
-    time0: sc.Variable,
+    chopper_delay: sc.Variable,
 ) -> sc.Variable:
-    out = t0.copy()
-    out -= time0
+    out = t0 - chopper_delay
     out /= mod_period
     out += 0.5
     sc.floor(out, out=out)
     out *= mod_period
-    out += time0
+    out += chopper_delay
     return out
 
 
@@ -152,13 +151,13 @@ def _tof_from_dhkl(
     coarse_dhkl: sc.Variable,
     Ltotal: sc.Variable,
     mod_period: sc.Variable,
-    time0: sc.Variable,
+    chopper_delay: sc.Variable,
 ) -> sc.Variable:
     """Computes tof for BEER given the dhkl peak that the event belongs to"""
     # Source: https://www.mcstas.org/download/components/current/contrib/NPI_tof_dhkl_detector.comp
     # tref = 2 * d_hkl * sin(theta) / hm * Ltotal
-    # tc = time_of_arrival - time0 - tref
-    # dt = floor(tc / mod_period + 0.5) * mod_period + time0
+    # tc = time_of_arrival - chopper_delay - tref
+    # dt = floor(tc / mod_period + 0.5) * mod_period + chopper_delay
     # tof = time_of_arrival - dt
     c = (-2 * 1.0 / (scipp.constants.h / scipp.constants.m_n)).to(
         unit=f'{time_of_arrival.unit}/m/angstrom'
@@ -167,12 +166,12 @@ def _tof_from_dhkl(
     out *= sc.sin(theta)
     out *= Ltotal
     out += time_of_arrival
-    out -= time0
+    out -= chopper_delay
     out /= mod_period
     out += 0.5
     sc.floor(out, out=out)
     out *= mod_period
-    out += time0
+    out += chopper_delay
     out *= -1
     out += time_of_arrival
     return out
@@ -185,7 +184,7 @@ def geometry_graph() -> GeometryCoordTransformGraph:
 def tof_from_known_dhkl_graph(
     mod_period: ModulationPeriod,
     pulse_length: PulseLength,
-    time0: WavelengthDefinitionChopperDelay,
+    chopper_delay: WavelengthDefinitionChopperDelay,
     dhkl_list: DHKLList,
     gg: GeometryCoordTransformGraph,
 ) -> TofCoordTransformGraph:
@@ -217,7 +216,7 @@ def tof_from_known_dhkl_graph(
         **graph.tof.elastic("tof"),
         'pulse_length': lambda: pulse_length,
         'mod_period': lambda: mod_period,
-        'time0': lambda: time0,
+        'chopper_delay': lambda: chopper_delay,
         'tof': _tof_from_dhkl,
         'time_of_arrival': time_of_arrival,
         'coarse_dhkl': _compute_coarse_dspacing,
