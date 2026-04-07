fix: remove theta masking during load

tests/beer/mcstas_reduction_test.py
@@ -22,7 +22,7 @@ from ess.beer.io import (
     mcstas_chopper_delay_from_mode_new_simulations,
 )
 from ess.beer.types import DetectorBank, DHKLList, WavelengthDetector
-from ess.powder.types import SampleRun
+from ess.powder.types import ElasticCoordTransformGraph, SampleRun
 from ess.reduce.nexus.types import Filename
 
 
@@ -77,12 +77,15 @@ def test_pulse_shaping_workflow():
     wf = BeerMcStasWorkflowPulseShaping()
     wf[Filename[SampleRun]] = mcstas_silicon_new_model(6)
     wf[DetectorBank] = DetectorBank.north
-    da = wf.compute(WavelengthDetector[SampleRun])
+    res = wf.compute(
+        (WavelengthDetector[SampleRun], ElasticCoordTransformGraph[SampleRun])
+    )
+    da = res[WavelengthDetector[SampleRun]]
     assert 'wavelength' in da.bins.coords
     # assert dataarray has all coords required to compute dspacing
     da = da.transform_coords(
         ('dspacing',),
-        graph=scn.conversion.graph.tof.elastic('tof'),
+        graph=res[ElasticCoordTransformGraph[SampleRun]],
     )
     h = da.hist(dspacing=2000, dim=da.dims)
     max_peak_d = sc.midpoints(h['dspacing', np.argmax(h.values)].coords['dspacing'])[0]